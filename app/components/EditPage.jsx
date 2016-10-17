47d9313565c84e8bb486ebc416f50365f2088cf2
skbolton <stephenkbolton@gmail.com>
2016-10-16 23:13:52 -0500

    Finish basic EditPage component

---
+++ app/components/EditPage.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const Queue = require('./Queue');
+const Favorites = require('./Favorites');
+
+const EditPage = (props) => {
+  return (
+    <div className="row">
+      <Queue queue={props.queue} />
+      <Favorites favorites={props.favorites} />
+    </div>
+  );
+};
+
+module.exports = EditPage;
