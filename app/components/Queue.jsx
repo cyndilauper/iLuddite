61639d74eb5c74c8c0441d0a6e743583927394cd
skbolton <stephenkbolton@gmail.com>
2016-10-16 22:36:10 -0500

    Finish basic Queue component.

---
+++ app/components/Queue.jsx
@@ -0,0 +1,35 @@
+const React = require('react');
+const QueueListItem = require('./QueueListItem');
+
+const Queue = (props) => {
+  // props.queue is a list of all the users books in their queue
+  // the first one in the queue is their currently reading book 
+  // so it needs different styling.
+  const queue = props.queue.map((book, idx) => {
+    if (idx === 0) {
+      return (
+        <QueueListItem 
+          currentBook={true}
+          book={book}
+        />
+      );
+    } else {
+      return (
+        <QueueListItem
+          currentBook={false}
+          book={book}
+        />
+      );
+    }
+  });
+  return (
+    <div className="col-md-6">
+      <h2>Queue:</h2>
+      <ul>
+        {queue}
+      </ul>
+    </div>
+  );
+};
+
+module.exports = Queue;
