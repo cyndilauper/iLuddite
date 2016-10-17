dc327da34c45a9ed8922e31bc2a58fb638ea1102
skbolton <stephenkbolton@gmail.com>
2016-10-16 22:05:22 -0500

    Add folder to store frontend components

---
+++ app/components/App.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+
+class App extends React.Component {
+  render () {
+    return (
+      <h1>Hello iLuddite!</h1>
+    );
+  }
+}
+
+// export the class so other files can work with it.
+module.exports = App;
