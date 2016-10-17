3e0d0e0549699ccd4741aadaa92f9a188bddee8f
skbolton <stephenkbolton@gmail.com>
2016-10-16 22:06:08 -0500

    Set up boilerplate for how to require in other components and render them.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,11 @@
 /*
   This is the entry point for the front end. Do not rename or move this file without changing the "entry" property inside of webpack.config.js
 */
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// this is all just an example of how to import other components
+const App = require('./components/App');
+
+// and then render them out
+ReactDOM.render(<App />, document.getElementById('app'));
