275a2334306465ebec171e299a66fad951415b7c
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-17 18:44:19 -0500

    auth check on homepage

--- server/app.js
+++ server/app.js
@@ -17,7 +17,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({secret: 'booksyall'}));
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({index: 'test'});
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+//this should work
+
+function authCheck(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+router.get('/', authCheck, (req, res, next) => {
+  res.json({index: 'this should only be viewable by logged in users'});
+})
+
+router.get('/login', (req, res, next) => {
+  res.send('to view this site you must <a href="./auth/facebook">login with fb</a>')
 })
 
 module.exports = router;
