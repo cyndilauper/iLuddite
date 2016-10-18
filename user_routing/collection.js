a65cd8ccc17f9681310b2567e932312c343c5fc3
Rachel DePriest <racheldepriest1@gmail.com>
2016-10-17 19:15:29 -0500

    Created user routing folder

---
+++ user_routing/collection.js
@@ -0,0 +1,15 @@
+//user_collection
+
+var mongoose= require('mongoose')
+var router= express.Router();
+
+var userSchema = mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
+  currentBook:{type: String},
+  favoriteBooks:{type: Number}
+});
+
+
+
+var User = mongoose.model('User', userSchema);
