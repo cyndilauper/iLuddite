83cc151cc05b3d5edb464b60e4201a867912e4f0
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-18 11:08:45 -0500

    removed cookie-parser and fixed session warnings

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
@@ -16,9 +15,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
 
-app.use(session({secret: 'booksyall'}));
+app.use(session({
+    secret: 'booksyall',
+    resave: true,
+    saveUninitialized: true
+}));
 
 require('./config/passport')(app);
 
