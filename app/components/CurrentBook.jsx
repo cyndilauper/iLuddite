b91789d2bda77c7fb2c3d0bd340fe8011651fbea
Joey Steinberger <josamuel@umich.edu>
2016-10-17 19:37:13 -0500

    adding current books component

---
+++ app/components/CurrentBook.jsx
@@ -0,0 +1,38 @@
+const React = require('react');
+
+
+const CurrentBook = (props) => {
+  return (
+           <div className="col-sm-6 currentColumn">
+                <div className="row">
+                    <div className="col-xs-12 currentHeader">
+                        Currently Reading
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-4 currentPhotoColumn">
+                        <a href="#"><img src={props.book.image}className="img-responsive bookPhoto"/></a>
+                    </div>
+                    <div className="col-xs-8 currentInfoColumn">
+                        <div className="row">
+                            <div className="col-xs-12 currentTitle">
+                                {props.book.title}
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-12 currentAuthor">
+                                {props.book.author}
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-12 currentBio">
+                                {props.book.description}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+  );
+};
+
+module.exports = CurrentBook;
