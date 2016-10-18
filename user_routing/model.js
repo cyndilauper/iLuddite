a85c73bd385bcced681877d4c26fff11b26fead5
Rachel DePriest <racheldepriest1@gmail.com>
2016-10-17 19:21:42 -0500

    Working on user schema

--- user_routing/model.js
+++ user_routing/model.js
@@ -7,9 +7,15 @@ var userSchema = mongoose.Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
   currentBook:{type: String},
+  bookQueue:{type: String},
+  friendsList:{type: String},
   favoriteBooks:{type: Number}
 });
 
 
 
 var User = mongoose.model('User', userSchema);
+
+User.
+
+module.exports= User;
