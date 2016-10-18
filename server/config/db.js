3513ecfbc12fa77ed6d80d5b20a1a50e85ee7d84
Brittany Artimez <brittanyartimez@gmail.com>
2016-10-18 16:31:31 -0500

    connected mLab & added credentials to gitignore

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
+var creds = require('./mongo');
 // mongoDB connection settings
-// const connection = `mongodb://${process.env.dbHost}/${process.env.dbName}`;
-const connection = 'mongodb://localhost:27017/iLuddite';
+const connection = `mongodb://${creds.dbHost}/${creds.dbName}`;
+// const connection = 'mongodb://localhost:27017/iLuddite';
 const mongoose = require('mongoose');
 mongoose.connect(connection);
 var db = mongoose.connection;
