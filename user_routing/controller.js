97c4a0a7f508fdde388bffb691e7e3ab7859cbd5
Rachel DePriest <racheldepriest1@gmail.com>
2016-10-17 20:06:40 -0500

    Working on router.put

--- user_routing/controller.js
+++ user_routing/controller.js
@@ -12,16 +12,38 @@ router.use(methodOverride(function(req, res) {
 	}
 }))
 
-router.get('/:userid/edit', function(req, res){
-	mongoose.model('Users').findById(req.id, function(err, user){
-		if(err){
+router.get('/:userid/edit', function(req, res) {
+	mongoose.model('Users').findById(req.id, function(err, user) {
+		if (err) {
 			console.log('You have a GET Error: There was a problem getting:' + err);
 		} else {
 			console.log('GET retrived ID:' + user._id);
 		}
-	}) 
+	})
 })
 
+router.put('/:userid/', function(req, res){
+	var username= req.body.username;
+	var currentBook= req.body.currentBook;
+	var bookQueue= req.body.bookQueue;
+	var friendsList= req.body.friendsList;
+	var favoriteBooks= req.body.favoriteBooks; 
+
+	mongoose.model('Users').findById(req.id, function (error, user){
+		user.update({
+			username: username,
+			currentBook: currentBook,
+			bookQueue: bookQueue,
+			favoriteBooks: favoriteBooks
+		} function(err, userId){
+			if(err){
+				res.send("There was a problem updating the information to the database:" err);
+			} else{
+				
+			}
+		})
+	})
+})
 
 
 module.exports = router;
