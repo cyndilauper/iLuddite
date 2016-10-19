7a07748beb35f5efb44a09b331a402cad7c2bb36
skbolton <stephenkbolton@gmail.com>
2016-10-19 16:12:17 -0500

    Add comments for clarity

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -22,9 +22,13 @@ class App extends React.Component {
     );
   }
 
+  // This function is used to render out children given to App by router
+  // before rendering them we inspect what type of component they are
+  // and inject properties into them so that they can display all the data
+  // they need.
   renderChildrenWithProps () {
-    // loop over the this.props.children and check the childs type
-    // if it is an EditPage component give them our queue and favorites
+    // loop through the children of App and add properties to component
+    // and return a copy of it with new props.
     return React.Children.map(this.props.children, (child) => {
       if (child.type.name === "EditPage") {
         return React.cloneElement(child, {
