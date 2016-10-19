6fca2627aefb0a6b7943a405a7e8e73623af5476
skbolton <stephenkbolton@gmail.com>
2016-10-18 22:06:08 -0500

    Add dummy data for EditPage component.

--- app/index.js
+++ app/index.js
@@ -6,6 +6,42 @@ const ReactDOM = require('react-dom');
 
 // this is all just an example of how to import other components
 const App = require('./components/App');
+const EditPage = require('./components/EditPage');
+
+// dummy data for EditPage
+const data = {
+  queue: [
+    {
+      title: 'Of Mice and Men',
+      author: 'John Steinbeck'
+    },
+    {
+      title: 'A Tale of Two Cities',
+      author: 'Charles Dickens'
+    },
+    {
+      title: 'Little Women',
+      author: 'Louisa May Alcott'
+    }
+  ],
+  favorites: [
+    {
+      title: 'Of Mice and Men',
+      author: 'John Steinbeck'
+    },
+    {
+      title: 'A Tale of Two Cities',
+      author: 'Charles Dickens'
+    },
+    {
+      title: 'Little Women',
+      author: 'Louisa May Alcott'
+    }
+  ]
+}
 
 // and then render them out
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <EditPage favorites={data.favorites} queue={data.queue} />, 
+  document.getElementById('app')
+);
