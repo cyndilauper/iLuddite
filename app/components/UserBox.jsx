5fc5b69669a1a57dbd8209d4bd8e5177a09d39c2
Joey Steinberger <josamuel@umich.edu>
2016-10-18 19:51:01 -0500

    fixed userBox indenting

--- app/components/UserBox.jsx
+++ app/components/UserBox.jsx
@@ -1,31 +1,30 @@
 const React = require('react');
 
-const UserBox = (user) => {
+const UserBox = (props) => {
   return (
-
-        <div className="col-sm-6 profileColumn">
-        <div className="row">
-            <div className="col-xs-7 profilePhotoColumn">
-                <img src={user.image} className="img-responsive profilePhoto"/>
-            </div>
-            <div className="col-xs-5 userInfoColumnn">
-                <div className="row">
-                    <div className="col-xs-12 userName">
-                        {user.name}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-12 userLocation">
-                         {user.location}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-12 userBooksRead">
-                        {user.bookCount}
-                    </div>
-                </div>
-            </div>
+    <div className="col-sm-6 profileColumn">
+      <div className="row">
+        <div className="col-xs-7 profilePhotoColumn">
+          <img src={props.user.image} className="img-responsive profilePhoto"/>
         </div>
+        <div className="col-xs-5 userInfoColumnn">
+          <div className="row">
+            <div className="col-xs-12 userName">
+              {props.user.name}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12 userLocation">
+               {props.user.location}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12 userBooksRead">
+              {props.user.bookCount}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
