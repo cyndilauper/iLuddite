bfe27a9de8bc74ff4661cd962dfa6909374d0959
Rachel DePriest <racheldepriest1@gmail.com>
2016-10-19 15:42:31 -0500

    Post request for users books finished

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../users');
+const mongoose = require('mongoose');
+const Books = require('./books');
+const Users = require('./users');
 
 
 // router.param('userid', (req, res, next, userid) => {
@@ -24,37 +24,69 @@ router.get('/:userid', (req, res, next) => {
 
 router.get('/:userid/queue', (req, res, next) => {
   // GET user queue
-  if(err){
+  if (err) {
     console.log('You have a GET Error: There was a problem getting:' + err);
   } else {
-    
+    console.log('GET retrived queue for:' + user._id);
   }
-
   res.json({
     'userIDqueue': req.params.userid
   });
+  next();
 })
 
 router.route('/:userid/queue/:bookid')
   // POST or DELETE book to queue
   .post((req, res, next) => {
-    if (req.param('current') === 'true') {
-      // if request ends in current=true, push to top of list
-      res.json({
-        'bookIDz': req.params.bookid
+      if (req.param('current') === 'true') {
+        // if request ends in current=true, push to top of list
+        var queue = [{
+            "schema": require('/models/book.js'),
+            "query": {
+              bookid: req.body.bookid
+            }
+          }, {
+            "schema": require('/models/user.js'),
+            "query": {
+              username: req.body.userid
+            }
+          }],
+          bookData = [];
+
+        var currentBook = 0.
+
+
+        function recurse() {
+          if (currentBook < queue.length) {
+            var task = queue[currentBook];
+            task.schema.findOne(task.query, function(err, data) {
+              bookData.push(err || data);
+              currentBook++;
+              recurse();
+            })
+          } else {
+            res.json(bookData);
+          }
+        }
+        recurse();
+        next();
       });
-    } else {
-      // else, post to bottom
-      res.json({
-        'bookID': req.params.bookid
-      });
-    }
-  })
-  .delete((req, res, next) => {
     res.json({
-      'bookID': req.params.bookid
+      'bookIDz': req.params.bookid
     });
-  })
+  }
+else {
+  // else, post to bottom
+  res.json({
+    'bookID': req.params.bookid
+  });
+}
+})
+.delete((req, res, next) => {
+  res.json({
+    'bookID': req.params.bookid
+  });
+})
 
 router.route('/:userid/favorites')
   // GET, POST, or DELETE user's favorite books
