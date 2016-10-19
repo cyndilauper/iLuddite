cd58c6c584af250231be412768117b174e5717fe
Joey Steinberger <josamuel@umich.edu>
2016-10-18 21:49:41 -0500

    adding currentBook tests to project

--- app/index.js
+++ app/index.js
@@ -8,4 +8,19 @@ const ReactDOM = require('react-dom');
 const App = require('./components/App');
 
 // and then render them out
+
+var currentBook = {
+  image: '/assets/book0.jpg',
+  title: 'The MotorCycle Diaries',
+  author: 'Che Guevera',
+  description: 'The Motorcycle Diaries (Spanish: Diarios de Motocicleta) is a memoir that traces the early travels of Marxist revolutionary Ernesto "Che" Guevara, \
+  then a 23-year-old medical student, and his friend Alberto Granado, a 29-year-old biochemist. Leaving Buenos Aires, Argentina, in January 1952 on the back of a \
+  sputtering single cylinder 1939 Norton 500cc dubbed La Poderosa ("The Mighty One"), they desired to explore the South America they only knew from books.'
+}
+
+// to test currentBook set write const App = require('./components/CurrentBook');
+// and render: ReactDOM.render(<App currentBook={currentBook}/>, document.getElementById('app'));
+
+
+
 ReactDOM.render(<App />, document.getElementById('app'));
