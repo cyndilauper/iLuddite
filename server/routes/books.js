094a3911f93d44b577a2a3e2a665977e5c955e28
Brittany Artimez <brittanyartimez@gmail.com>
2016-10-19 16:29:34 -0500

    insertion of 5 searched books into db working

--- server/routes/books.js
+++ server/routes/books.js
@@ -18,31 +18,39 @@ router.get('/', (req, res, next) => {
 
 //endpoint for navbar search of 3P API by title
 router.get('/search/:searchterm', (req, res) => {
-  //hard-coding req.params.title for now.
-  // req.params.searchterm = "Kavalier and Clay";
-  // var titleSearched = req.params.searchterm;
-  // console.log('req.params:', req.params);
+  var titleSearched = req.params.searchterm;
   var options = {
     url: 'https://www.googleapis.com/books/v1/volumes?q=' + titleSearched + '&key=' + google
   }
   function callback(err, resp, body) {
     if (!err && res.statusCode == 200) {
       var allResult = JSON.parse(body);
-      var firstBook = allResult.items[0];
-      console.log(firstBook);
+      // var firstBook = allResult.items[0];
       var firstFiveBooks = allResult.items.slice(0,6);
 
       //shape the data returned for the first five books for the navbar and for insertion into the db
       var shapedFiveBooks = firstFiveBooks.map(function(book){
+        console.log(book.volumeInfo.industryIdentifiers);
+        // var isbnTen = book.volumeInfo.industryIdentifiers
+
         return {
           title: book.volumeInfo.title,
           author: book.volumeInfo.authors,
           summary: book.volumeInfo.description,
           //we use the ISBN-13
-          isbns: book.volumeInfo.industryIdentifiers,
-          thumbnail: book.volumeInfo.imageLinks.thumbnail,
-          smallThumbnail: book.volumeInfo.imageLinks.smallThumbnail
+          isbn: (book.volumeInfo.industryIdentifiers)[0].identifier,
+          //the imageLinks (formerly thumbnail) property is behaving very strangely. book.volumeInfo.imageLinks is an object with two properties, 'smallThumbnail' and 'thumbnail', and inserts fine this way, but book.volumeInfo.imageLinks.thumbnail throws an error "cannot read property thumbnail of undefined"
+          imageLinks: book.volumeInfo.imageLinks
+          // smallThumbnail: book.volumeInfo.imageLinks.smallThumbnail
         }
+      })
+
+      //insert the five shaped books into the db
+      shapedFiveBooks.forEach(function(book, idx){
+        Books.create(book, function(err) {
+          if (err) console.log('book insert err:', err);
+          else console.log('book' + idx + ' inserted!');
+        })
       })
       res.send(shapedFiveBooks);
     }
