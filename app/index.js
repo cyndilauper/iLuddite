5e3010c25fe14def861839f9792c94b181f2830b
skbolton <stephenkbolton@gmail.com>
2016-10-18 22:35:13 -0500

    Move all data to one large object.

--- app/index.js
+++ app/index.js
@@ -9,8 +9,8 @@ const ReactDOM = require('react-dom');
 const App = require('./components/App');
 const EditPage = require('./components/EditPage');
 
-// dummy data for EditPage
-const data = {
+// dummy data all components
+const allData = {
   queue: [
     {
       title: 'Of Mice and Men',
@@ -25,6 +25,7 @@ const data = {
       author: 'Louisa May Alcott'
     }
   ],
+
   favorites: [
     {
       title: 'Of Mice and Men',
@@ -38,46 +39,34 @@ const data = {
       title: 'Little Women',
       author: 'Louisa May Alcott'
     }
-  ]
+  ],
+
+  user: {
+    image: '/assets/user.jpg',
+    name: 'Luddington Beasley',
+    location: 'Austin, Tx',
+    booksRead: '43'
+  },
+
+  currentBook: {
+    image: '/assets/book0.jpg',
+    title: 'The MotorCycle Diaries',
+    author: 'Che Guevera',
+    description: 'The Motorcycle Diaries (Spanish: Diarios de Motocicleta) is a memoir that traces the early travels of Marxist revolutionary Ernesto "Che" Guevara, \
+    then a 23-year-old medical student, and his friend Alberto Granado, a 29-year-old biochemist. Leaving Buenos Aires, Argentina, in January 1952 on the back of a \
+    sputtering single cylinder 1939 Norton 500cc dubbed La Poderosa ("The Mighty One"), they desired to explore the South America they only knew from books.'
+  },
+
+  book: {
+    image: '/assets/kavclay.jpg',
+    title: 'The Amazing Adventures of Kavalier & Clay',
+    author: 'Michael Chabon',
+    authorDescription: 'Michael Chabon is a totally rad dude who writes books that will break your heart.',
+    description: 'Kav & Clay is probably the best book I\'ve read in the last five years.  Blah blah blah blah Donec id elit non mi porta gravida at eget metus. \
+     Maecenas faucibus mollis interdum.  Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.'
+  }
 }
-
-// and then render them out
-
-
-var user = {
-  image: '/assets/user.jpg',
-  name: 'Luddington Beasley',
-  location: 'Austin, Tx',
-  booksRead: '43'
-}
-
-// to test user set const App = require('./components/UserBox');
-// and  ReactDOM.render(<App user={user}/>, document.getElementById('app'));
-
-var currentBook = {
-  image: '/assets/book0.jpg',
-  title: 'The MotorCycle Diaries',
-  author: 'Che Guevera',
-  description: 'The Motorcycle Diaries (Spanish: Diarios de Motocicleta) is a memoir that traces the early travels of Marxist revolutionary Ernesto "Che" Guevara, \
-  then a 23-year-old medical student, and his friend Alberto Granado, a 29-year-old biochemist. Leaving Buenos Aires, Argentina, in January 1952 on the back of a \
-  sputtering single cylinder 1939 Norton 500cc dubbed La Poderosa ("The Mighty One"), they desired to explore the South America they only knew from books.'
-}
-
-// to test currentBook set write const App = require('./components/CurrentBook');
-// and render: ReactDOM.render(<App currentBook={currentBook}/>, document.getElementById('app'));
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
-var book = {
-  image: '/assets/kavclay.jpg',
-  title: 'The Amazing Adventures of Kavalier & Clay',
-  author: 'Michael Chabon',
-  authorDescription: 'Michael Chabon is a totally rad dude who writes books that will break your heart.',
-  description: 'Kav & Clay is probably the best book I\'ve read in the last five years.  Blah blah blah blah Donec id elit non mi porta gravida at eget metus. \
-   Maecenas faucibus mollis interdum.  Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.'
-}
-
-//to test book write const App = require('./components/Book'); 
-// and render ReactDOM.render(<App book={book}/>, document.getElementById('app'));
 
