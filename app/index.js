1bf432ae76d7cc35259baec25339f0b00235ef43
skbolton <stephenkbolton@gmail.com>
2016-10-19 16:07:55 -0500

    Add router logic to index to start hooking up routes.

--- app/index.js
+++ app/index.js
@@ -10,14 +10,18 @@ import { Router, Route, browserHistory } from 'react-router';
 // this is all just an example of how to import other components
 
 const App = require('./components/App');
-
 const Landing = require('./components/Landing');
+const EditPage = require('./components/EditPage');
+const dummyData = require('./dummyData');
 
 // and then render them out
 ReactDOM.render(
-  <Router>
+  <Router history={browserHistory}>
     <Route path="/" component={Landing} />
-    <Route path="/profile" component={App} />
+    <Route path="/users/:userid" component={App} data={dummyData}>
+      <Route path="edit" component={EditPage} />
+    </Route>
+
   </Router>,
   document.getElementById('app')
 );
