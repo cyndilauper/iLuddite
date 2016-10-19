2c8306d957407af1d33d93a53127553578a9e738
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-18 19:49:26 -0500

    rolling back for now, as this method loses friends connection

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,25 +6,25 @@ module.exports = function (app) {
   app.use(passport.initialize());
   app.use(passport.session());
 
+  passport.serializeUser((user, done) => {
+      done(null, user)
+  });
+
+  passport.deserializeUser((user, done) => {
+      done(null, user);
+  });
+
   // passport.serializeUser((user, done) => {
-  //     done(null, user)
+  //     done(null, user.id)
   // });
   //
-  // passport.deserializeUser((user, done) => {
+  // passport.deserializeUser((id, done) => {
+  //   User.findOne({id}).then(user => {
   //     done(null, user);
+  //   }).catch(err => {
+  //     throw err;
+  //   })
   // });
-
-  passport.serializeUser((user, done) => {
-      done(null, user.id)
-  });
-
-  passport.deserializeUser((id, done) => {
-    User.findOne({id}).then(user => {
-      done(null, user);
-    }).catch(err => {
-      throw err;
-    })
-  });
 
   require('./strategies/facebook')();
 
