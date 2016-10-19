914cb3887314657ac5378543574ff739828caee5
skbolton <stephenkbolton@gmail.com>
2016-10-18 19:19:26 -0500

    Fix indentation

--- app/components/CurrentBook.jsx
+++ app/components/CurrentBook.jsx
@@ -3,35 +3,35 @@ const
 
 const CurrentBook = (book) => {
   return (
-           <div className="col-sm-6 currentColumn">
+    <div className="col-sm-6 currentColumn">
+      <div className="row">
+        <div className="col-xs-12 currentHeader">
+            Currently Reading
+        </div>
+      </div>
+        <div className="row">
+            <div className="col-xs-4 currentPhotoColumn">
+                <a href="#"><img src={book.image}className="img-responsive bookPhoto"/></a>
+            </div>
+            <div className="col-xs-8 currentInfoColumn">
                 <div className="row">
-                    <div className="col-xs-12 currentHeader">
-                        Currently Reading
-                    </div>
+                  <div className="col-xs-12 currentTitle">
+                    {book.title}
+                  </div>
                 </div>
                 <div className="row">
-                    <div className="col-xs-4 currentPhotoColumn">
-                        <a href="#"><img src={book.image}className="img-responsive bookPhoto"/></a>
-                    </div>
-                    <div className="col-xs-8 currentInfoColumn">
-                        <div className="row">
-                            <div className="col-xs-12 currentTitle">
-                                {book.title}
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-xs-12 currentAuthor">
-                                {book.author}
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-xs-12 currentBio">
-                                {book.description}
-                            </div>
-                        </div>
-                    </div>
+                  <div className="col-xs-12 currentAuthor">
+                    {book.author}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-xs-12 currentBio">
+                    {book.description}
+                  </div>
                 </div>
             </div>
+        </div>
+    </div>
   );
 };
 
