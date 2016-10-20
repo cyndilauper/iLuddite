b0b5d0622b9db855e436be2dcf8f907db4461c83
Brittany Artimez <brittanyartimez@gmail.com>
2016-10-20 17:48:24 -0500

    deleted find by isbn static

--- server/models/books.js
+++ server/models/books.js
@@ -13,13 +13,5 @@ const booksSchema = new Schema({
   thumbnail: { data: Buffer, contentType: String}
 });
 
-booksSchema.statics.getBookByISBN = function(isbn){
-  Books.findOne({isbn: isbn}, function(err, book) {
-    if (err) console.log('get book error:', err);
-    //TODO: do stuff with the book
-    console.log(book);
-  })
-}
-
 var Books = mongoose.model('Books', booksSchema);
 module.exports = Books;
