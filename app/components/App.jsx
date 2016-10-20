f876e35f475dbe85a9c9c6ff745019fda0e71b63
skbolton <stephenkbolton@gmail.com>
2016-10-19 19:20:23 -0500

    Add initial state to app and functions to handle navbar searches

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,20 +1,56 @@
 const React = require('react');
 const Navbar = require('./Navbar');
 const EditPage = require('./EditPage');
+const axios = require('axios');
 
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      loggedInUserId: '',
+      loggedInUserQueue: [],
+      loggedInUserFavorites: [],
+      navbarSearchText: '',
+      navbarSearchResults: [1,1,1]
+    }
+  }
+
   render () {
+    const style = { height: '100vh' };
     return (
-      <div>
-        <Navbar />
+      <div style={style} onClick={this.clearSearchResults.bind(this)}>
+        <Navbar 
+          changeSearchText={this.changeSearchText.bind(this)}
+          searchText={this.state.navbarSearchText}
+          searchResults={this.state.navbarSearchResults}
+        />
         <div className="container">
           <EditPage 
+            queue={this.props.data.queue} 
             favorites={this.props.data.favorites}
-            queue={this.props.data.queue}
           />
         </div>
       </div>
     );
+  }
+
+  // allow navbar input field to change navbarSearchText
+  changeSearchText (newText) {
+    this.setState({
+      navbarSearchText: newText
+    });
+  }
+
+  // uses the navbarSearchText to do an api call and search for a book.
+  searchForBook () {
+    axios.get(`/books/search/${this.state.navbarSearchText}`)
+      .then(response => this.setState({ navbarSearchResults: response }));
+  }
+
+  clearSearchResults () {
+    this.setState({
+      navbarSearchResults: []
+    });
   }
 }
 
