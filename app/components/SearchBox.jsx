883110ef511eaca963fd6a8e1876fbea31b63b6a
skbolton <stephenkbolton@gmail.com>
2016-10-19 19:21:16 -0500

    edit searchBox

--- app/components/SearchBox.jsx
+++ app/components/SearchBox.jsx
@@ -1,34 +1,56 @@
 const React = require('react');
 const SearchListItem = require('./SearchListItem');
 
-const SearchBox = (props) => {
-  // if this component is given search results then it will render out
-  // a large list of search results
-  // otherwise it is simply an input box
-  if (props.searchResults) {
-    return (
-      <ul className="search-box">
-        <span className="glyphicon glyphicon-search"></span>
-        <input 
-          type="text"
-          placeholder="Search for Books"
-        />
-        <SearchListItem />
-        <SearchListItem />
-        <SearchListItem />
-      </ul>
-    );
-  } else {
-    return (
-      <div className="search-box">
-        <span className="glyphicon glyphicon-search"></span>
-        <input 
-          type="text"
-          placeholder="Search for Books"
-        />
-      </div>
-    );
+class SearchBox extends React.Component {
+  constructor (props) {
+    super(props);
   }
+
+  render () {
+    // if this component is given search results then it will render out
+    // a large list of search results
+    // otherwise it is simply an input box
+    if (this.props.searchResults.length) {
+      // make a SearchListItem for each book in searchResults
+      const searchResults = this.props.searchResults.map(book => (
+        <SearchListItem />
+      ))
+      return (
+        <ul className="search-box">
+          <span className="glyphicon glyphicon-search"></span>
+          <input 
+            type="text"
+            placeholder="Search for Books"
+            value={this.props.searchText}
+            onChange={this.handleTextChange.bind(this)}
+          />
+          {searchResults}
+        </ul>
+      );
+    } else {
+      return (
+        <div className="search-box">
+          <span className="glyphicon glyphicon-search"></span>
+          <input 
+            type="text"
+            placeholder="Search for Books"
+            value={this.props.searchText}
+            onChange={this.handleTextChange.bind(this)}
+          />
+        </div>
+      );
+    }
+  }
+
+  handleTextChange (evt) {
+    this.props.changeSearchText(evt.target.value);
+  }
+
+  // anytime the user moves away from the input clear
+  clearSearchText () {
+
+  }
+  
 }
 
 module.exports = SearchBox;
