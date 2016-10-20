90380059faaa1f0df2b94ad7f45f70c9b21a6e98
skbolton <stephenkbolton@gmail.com>
2016-10-19 19:32:37 -0500

    Add isbn numbers to dummy data.

--- app/dummyData.js
+++ app/dummyData.js
@@ -2,30 +2,41 @@ module.exports = {
   queue: [
     {
       title: 'Of Mice and Men',
-      author: 'John Steinbeck'
+      author: 'John Steinbeck',
+      isbn: 1
     },
     {
       title: 'A Tale of Two Cities',
-      author: 'Charles Dickens'
+      author: 'Charles Dickens',
+      isbn: 2
     },
     {
       title: 'Little Women',
-      author: 'Louisa May Alcott'
+      author: 'Louisa May Alcott',
+      isbin: 3
     }
   ],
 
   favorites: [
     {
       title: 'Of Mice and Men',
-      author: 'John Steinbeck'
+      author: 'John Steinbeck',
+      isbn: 1
     },
     {
       title: 'A Tale of Two Cities',
-      author: 'Charles Dickens'
+      author: 'Charles Dickens',
+      isbn: 2
     },
     {
       title: 'Little Women',
-      author: 'Louisa May Alcott'
+      author: 'Louisa May Alcott',
+      isbn: 3
+    },
+    {
+      title: 'The Power of Now',
+      author: 'Eckhardt Tolle',
+      isbn: 4
     }
   ],
 
@@ -42,7 +53,8 @@ module.exports = {
     author: 'Che Guevera',
     description: 'The Motorcycle Diaries (Spanish: Diarios de Motocicleta) is a memoir that traces the early travels of Marxist revolutionary Ernesto "Che" Guevara, \
     then a 23-year-old medical student, and his friend Alberto Granado, a 29-year-old biochemist. Leaving Buenos Aires, Argentina, in January 1952 on the back of a \
-    sputtering single cylinder 1939 Norton 500cc dubbed La Poderosa ("The Mighty One"), they desired to explore the South America they only knew from books.'
+    sputtering single cylinder 1939 Norton 500cc dubbed La Poderosa ("The Mighty One"), they desired to explore the South America they only knew from books.',
+    isbn: 5
   },
 
   book: {
@@ -51,7 +63,8 @@ module.exports = {
     author: 'Michael Chabon',
     authorDescription: 'Michael Chabon is a totally rad dude who writes books that will break your heart.',
     description: 'Kav & Clay is probably the best book I\'ve read in the last five years.  Blah blah blah blah Donec id elit non mi porta gravida at eget metus. \
-     Maecenas faucibus mollis interdum.  Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.'
+     Maecenas faucibus mollis interdum.  Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.',
+     isbn: 6
   },
 
   profileQueue: [ 
