59e61c5385e02f4df8ced31c928ceb4415ab6497
skbolton <stephenkbolton@gmail.com>
2016-10-20 22:27:22 -0500

    Fix syntax error in ProfileQueue that was slicing queue wrong.

--- app/components/ProfileQueue.jsx
+++ app/components/ProfileQueue.jsx
@@ -2,8 +2,8 @@ const React = require('react');
 
 const ProfileQueue = (props) => {
 
-
-const profileQueue = props.bookQueue.slice(1, 7).map((book, idx) => {
+// since the queue could get really long we only want to display up to 6 books
+const profileQueue = props.bookQueue.slice(0, 6).map((book, idx) => {
     return (
       <div className="col-md-2 col-sm-2 col-xs-2 readingColumn">
         <Link to="#"><img src={book.image} className='img-responsive bookPhoto'/></Link>
