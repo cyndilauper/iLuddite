bd757e8937ee1255d0ffc773c27b287e4857434a
Brittany Artimez <brittanyartimez@gmail.com>
2016-10-20 22:12:33 -0500

    books without base64

--- server/routes/books.js
+++ server/routes/books.js
@@ -12,10 +12,10 @@ router.get('/', (req, res, next) => {
   //returns all books
   Books.find({}, (err, books) => {
     if (err) {
-      res.statusCode(500).send(err);
+      res.send(err);
       console.log('find all books error:', err);
     }
-    else res.statusCode(200).send(books);
+    else res.send(books);
   })
 })
 
@@ -37,9 +37,7 @@ router.get('/search/:searchterm', (req, res) => {
           author: book.volumeInfo.authors[0],
           description: book.volumeInfo.description,
           coverPath: book.volumeInfo.imageLinks.thumbnail,
-          thumbnailPath: book.volumeInfo.imageLinks.thumbnail,
-          coverPhoto: { contentType: 'image/jpg' },
-          thumbnail: { contentType: 'image/jpg' }
+          thumbnailPath: book.volumeInfo.imageLinks.thumbnail
         };
       }
       catch(err) {
@@ -50,97 +48,26 @@ router.get('/search/:searchterm', (req, res) => {
         return book;
       }
     })
-    return five;
+    res.send(five);
   }
 
-  //okay here's where the magic begins.  make the starter API call to get the book list
+  //make the API call to get the books
   requestPromise(options)
   .then((rawBooks) => getBooks(rawBooks))
   .catch((err) => {
-    res.statusCode(500).send(err);
+    res.send(err);
     console.log('error in api call to google books:', err);
   })
-  .then((fiveBooks) => {
-    //mmmkay now we're going to get the cover photos for the books and store them
-    //this silly done function is req'd by the async module.  the second argument to async.map is an iteratee that takes as *its* second argument a (req'd) done callback
-    let done = (err, book) => book;
-    let booksWithCover = async.map(fiveBooks, (book, done) => {
-      // let coverPath = 'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-L.jpg';
-      let coverPath = book.coverPath;
-      let options = {
-        url: coverPath,
-        encoding: 'binary'
-      }
-
-      request(options, (err, res, body) => {
-        //pop that binary data into the book.coverPhoto.data property, son
-        if (!err && res.statusCode == 200) {
-          body = new Buffer(body, 'binary');
-          //convert into a base64 string
-          body = body.toString('base64');
-          book.coverPhoto.data = body;
-          done(null, book);
-        } else {
-          res.statusCode(500).send(err);
-          console.log('err inserting cover image data:', err);
-        }
-      })
-    }, function(err, result) {
-      if (err) {
-        res.statusCode(500).send(err);
-        console.log('err in first async map call:', err);
-      }
-      //the third argument to the async.map call is a function that does something with the result.  the .then chain from our original request-promise (rp) wasn't playing nicely so we're going to nest the call for the thumbnails here
-      async.map(result, (book, done) => {
-        // let thumbnailPath = 'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-S.jpg';
-        let thumbnailPath = book.thumbnailPath;
-        let options = {
-          url: thumbnailPath,
-          encoding: 'binary'
-        }
-
-        request(options, (err, res, body) => {
-          //pop that binary data into the book.thumbnail.data property, son
-          if (!err && res.statusCode == 200) {
-            body = new Buffer(body, 'binary');
-            //convert into a base64 string
-            body = body.toString('base64');
-            book.thumbnail.data = body;
-            done(null, book);
-          } else {
-            res.statusCode(500).send(err);
-            console.log('err inserting thumbnail image data:', err);
-          }
-        })
-      }, function(err, result) {
-        if (err) {
-          res.statusCode(500).send(err);
-          console.log('err in second async map call:', err);
-        }
-        //bomb.  same deal, this function is the third argument to the async.map that grabbed our thumbnails.  so 'result' here is errythang we need, primed for db insertion
-        Books.insertMany(result, (err, answer) => {
-          if (err) {
-            res.statusCode(500).send(err);
-            console.log('err inserting book to db:', err);
-          }
-          console.log(`Data Inserted:`, answer)
-        })
-        res.statusCode(200).send(result);
-      })
-    })
-  })
-  .catch((err) => res.statusCode(500).send(err))
-  //jesus, the nesting, it hurts.  sorry
 })
 
 //endpoint for retrieving books from db
 router.get('/:isbn', (req, res, next) => {
   Books.find({_id: req.params.isbn}, (err, book) => {
     if (err) {
-      res.statusCode(500).send(err);
+      res.send(err);
       console.log('error in finding book:', err);
     } else {
-      res.statusCode(200).send(book);
+      res.send(book);
     }
   })
 })
