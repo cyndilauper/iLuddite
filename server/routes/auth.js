292fe6a7a855f830d1d7376e77f60b6b478cd08f
skbolton <stephenkbolton@gmail.com>
2016-10-21 17:43:37 -0500

    Fix facebook auth to send json response to client with user info.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,10 +7,24 @@ router.route('/')
       scope: ['user_friends', 'user_location']
       // eventually add 'user_actions.books' to scope list
   }));
-router.route('/return')
-  .get(passport.authenticate('facebook', {
-      successRedirect: '/home',
-      failureRedirect: '/'
-  }));
+
+
+router.get('/return', (req, res, next) => {
+  passport.authenticate('facebook', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect('/');
+    }
+    req.logIn(user, err => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect(`/users/${user.fbid}`);
+      // return res.json(user);
+    });
+  })(req, res, next);
+});
 
 module.exports = router;
