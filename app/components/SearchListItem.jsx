0201c072570297456db60665630d266b6faf2662
skbolton <stephenkbolton@gmail.com>
2016-10-22 00:51:48 -0500

    Change SearchListItem component from static data to dynamic props.

--- app/components/SearchListItem.jsx
+++ app/components/SearchListItem.jsx
@@ -1,16 +1,16 @@
 const React = require('react');
 
 const SearchListItem = (props) => {
- 
+  console.log(props);
   return (
     <li className="search-list-item list-group-item">
-      <div className="row no-gutter">
+      <div className="row">
         <div className="col-md-1">
-          <img src="https://images.gr-assets.com/books/1355929358s/8921.jpg" alt=""/>
+          <img src={props.image} alt={props.title}/>
         </div>
         <div className="col-md-7">
-          <h4>Book Title <small> - Author name here </small></h4>
-          <p>This would be a quick description of the text.</p>
+          <h4>{props.title} <small> - {props.author} </small></h4>
+          <p>{props.summary}</p>
         </div>
         <div className="col-md-2">
           <button className="btn btn-primary">
