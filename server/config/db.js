fdf3d0d0b1f638414294d430568ea1d225994e42
Brittany Artimez <brittanyartimez@gmail.com>
2016-10-22 14:44:35 -0500

    Handled case in which book is already in the database

--- server/config/db.js
+++ server/config/db.js
@@ -13,18 +13,18 @@ db.on('error', function(err){ console.log('db err:', err) });
 db.once('open', function() {
   console.log('mongo connection open');
   //having weird issues with administrative rights on mongodb so added this to drop all collections on restart
-  mongoose.connection.db.dropCollection('users', function(err, result) {
-    if (err) console.log(err);
-    else console.log(result);
-  })
-  mongoose.connection.db.dropCollection('books', function(err, result) {
-    if (err) console.log(err);
-    else console.log(result);
-  })
-  mongoose.connection.db.dropCollection('authors', function(err, result) {
-    if (err) console.log(err);
-    else console.log(result);
-  })
+  // mongoose.connection.db.dropCollection('users', function(err, result) {
+  //   if (err) console.log(err);
+  //   else console.log(result);
+  // })
+  // mongoose.connection.db.dropCollection('books', function(err, result) {
+  //   if (err) console.log(err);
+  //   else console.log(result);
+  // })
+  // mongoose.connection.db.dropCollection('authors', function(err, result) {
+  //   if (err) console.log(err);
+  //   else console.log(result);
+  // })
   // Books.create(fakeBookData, function(err, books) {
   //   if (err) console.log('book data insert err:', err);
   //   console.log('books created:', books);
