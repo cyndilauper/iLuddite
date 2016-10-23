dfa701bd52bc8509108ca673887ac578e49eb2d1
skbolton <stephenkbolton@gmail.com>
2016-10-23 14:43:09 -0500

    pass removeBookFromFavorites to EditPage children.

--- app/components/FavoriteListItem.jsx
+++ app/components/FavoriteListItem.jsx
@@ -5,7 +5,10 @@ const FavoriteListItem = (props) => {
     <li className="list-group-item">
       <div className="row">
         <div className="col-md-1">
-          <span className="glyphicon glyphicon-remove"></span>
+          <span 
+            className="glyphicon glyphicon-remove"
+            onClick={props.removeBookFromFavorites.bind(null, props.book._id)}
+          />
         </div>
         <div className="col-md-10">
           {props.book.title} - {props.book.author}
