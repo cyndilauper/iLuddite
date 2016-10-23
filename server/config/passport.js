a0a819d9308314a5af4b23a36c9c70bc694c5a82
Brittany Artimez <brittanyartimez@gmail.com>
2016-10-23 16:32:17 -0500

    removed unneeded comments and console logs, added some comments for next folks

--- server/config/passport.js
+++ server/config/passport.js
@@ -14,18 +14,6 @@ module.exports = function(app) {
       done(null, user);
   });
 
-  // passport.serializeUser((user, done) => {
-  //     done(null, user.id)
-  // });
-  //
-  // passport.deserializeUser((id, done) => {
-  //   User.findOne({id}).then(user => {
-  //     done(null, user);
-  //   }).catch(err => {
-  //     throw err;
-  //   })
-  // });
-
   require('./strategies/facebook')();
 
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const path = require('path');
-
 const profile = require('../services/profile');
 
 function authCheck(req, res, next) {
