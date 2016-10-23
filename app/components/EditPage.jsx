6b2c07811347230bf24841f0facda880fbf23b52
skbolton <stephenkbolton@gmail.com>
2016-10-23 01:16:54 -0500

    Move EditPage component to be class component instead of Stateless function.

--- app/components/EditPage.jsx
+++ app/components/EditPage.jsx
@@ -2,13 +2,19 @@ const React = require('react');
 const Queue = require('./Queue');
 const Favorites = require('./Favorites');
 
-const EditPage = (props) => {
-  return (
-    <div className="row">
-      <Queue queue={props.queue} />
-      <Favorites favorites={props.favorites} />
-    </div>
-  );
+class EditPage extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+  
+  render () {
+    return (
+      <div className="row">
+        <Queue queue={this.props.queue} />
+        <Favorites favorites={this.props.favorites} />
+      </div>
+    );
+  }
 };
 
 EditPage.defaultProps = {
