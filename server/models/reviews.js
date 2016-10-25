0638e9e5f99b684e4e088020299a010e1470568c
Jason Barnett <jason@thebarnetts.ws>
2016-10-24 19:06:07 -0500

    adds date to review schema

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 
 const reviewsSchema = new Schema({
   _id: { type: String, required: true, unique: true },
-  book_id: { type: Schema.Types.ObjectId, ref: 'Books' },
-  user_id: { type: Schema.Types.ObjectId, ref: 'Users' },
-  content: String
+  book_id: { type: Schema.Types.String, ref: 'Books' },
+  user_id: { type: Schema.Types.String, ref: 'Users' },
+  content: String,
+  created_at: {type: Date, default: Date.now}
 });
 
 const Reviews = mongoose.model('Reviews', reviewsSchema);
