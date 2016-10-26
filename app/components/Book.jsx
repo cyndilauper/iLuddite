fce121a8d0ab5a69b49fd39626236993e121ef6b
Phil <plmok61@gmail.com>
2016-10-26 14:52:05 -0500

    removes link to the author is the authorId is undefined

--- app/components/Book.jsx
+++ app/components/Book.jsx
@@ -44,6 +44,9 @@ class Book extends React.Component {
   }
 
   render () {
+    // if (this.state.book.authorId) {
+
+    // }
     const { addBookToQueue, addBookToFavorites, makeCurrentBook, addBookToPastReads } = this.props;
     return (
       <div className="bookContainer">
@@ -56,9 +59,13 @@ class Book extends React.Component {
             <h2>{this.state.book.title}</h2>
 
             <h3>
-              <Link to={`/authors/${this.state.book.authorId}`} >
-                {this.state.book.author}
-              </Link>
+              {+this.state.book.authorId ?
+                (<Link to={`/authors/${this.state.book.authorId}`} >
+                  {this.state.book.author}
+                </Link>) :
+
+                  this.state.book.author
+              }
             </h3>
             <h4>About the Book</h4>
             <p>{this.state.book.summary}</p>
