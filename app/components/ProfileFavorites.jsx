3198c50a8f8f8c90167866fc96291b114c977c70
charlieperson <charlie.person@hyperisland.se>
2016-10-25 23:07:29 -0500

    Creates drops downs for queue, pastReads, and favorites on profile page

--- app/components/ProfileFavorites.jsx
+++ app/components/ProfileFavorites.jsx
@@ -1,33 +1,22 @@
 const React = require('react');
 import { Link } from 'react-router';
+import { DropdownButton } from 'react-bootstrap';
 
 const ProfileFavorites = (props) => {
 // since the queue could get really long we only want to display up to 6 books
 const profileFavorites = props.favorites.slice(0, 6).map((book, idx) => {
     return (
-      <div className="col-md-2 col-sm-2 col-xs-2 readingColumn">
-        <Link to={`/books/${book._id}`}>
-          <img src={book.thumbnailPath} className='img-responsive bookPhoto'/>
-        </Link>
-      </div>
+      <Link to={`/books/${book._id}`}>
+        <img src={book.thumbnailPath} className='img-responsive bookPhoto'/>
+      </Link>
     );
   });
 
   return(
-   <div className ="wrappingDiv">
-      <div className="row readingTitleRow">
-        <div className="col-xs-12 readingHeader">
-          Favorites
-        </div>
-      </div>
-      <div className="row readingRow">
-        <div className="col-xs-12">
-          <div className="row">
-            {profileFavorites}
-          </div>
-        </div>
-      </div>
-    </div>
+    <DropdownButton title="Favs">
+      {profileFavorites}
+    </DropdownButton>
+
   );
 
 };
--- app/components/ProfileQueue.jsx
+++ app/components/ProfileQueue.jsx
@@ -1,35 +1,23 @@
 const React = require('react');
 import { Link } from 'react-router';
+import { DropdownButton } from 'react-bootstrap';
 
 const ProfileQueue = (props) => {
 // since the queue could get really long we only want to display up to 6 books
 const profileQueue = props.bookQueue.slice(0, 6).map((book, idx) => {
     return (
-      <div className="col-md-2 col-sm-2 col-xs-2 readingColumn">
-        <Link to={`/books/${book._id}`}>
-          <img src={book.thumbnailPath} className='img-responsive bookPhoto'/>
-        </Link>
-      </div>
+      <Link to={`/books/${book._id}`}>
+        <img src={book.thumbnailPath} className='img-responsive bookPhoto'/>
+      </Link>
     );
   });
 
   console.log(props.bookQueue)
 
   return(
-   <div className ="wrappingDiv">
-      <div className="row readingTitleRow">
-        <div className="col-xs-12 readingHeader">
-          Reading Queue
-        </div>
-      </div>
-      <div className="row readingRow">
-        <div className="col-xs-12">
-          <div className="row">
-            {profileQueue}
-          </div>
-        </div>
-      </div>
-    </div>
+    <DropdownButton title="Queue">
+      {profileQueue}
+    </DropdownButton>
   );
 
 };
