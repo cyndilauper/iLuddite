714ccb786468a4240cfcb97a049a8aa33e73db69
Scott <scotthorn0@gmail.com>
2016-10-25 20:11:05 -0500

    Search bar work in progress

--- app/components/SearchListItem.jsx
+++ app/components/SearchListItem.jsx
@@ -3,39 +3,49 @@ import { Link } from 'react-router';
 
 const SearchListItem = (props) => {
   return (
-    <li className="search-list-item list-group-item">
       <div className="row">
-        <div className="col-md-1">
+      <li className="search-list-item list-group-item">
+        <div className="col-xs-1">
           <Link to={`/books/${props.bookid}`}>
             <img src={props.image} alt={props.title}/>
           </Link>
         </div>
-        <div className="col-md-7">
+        <div className="col-xs-8">
           <Link to={`/books/${props.bookid}`}>
-            <h4>{`${props.title.substr(0, 50)}`}<br /><small>by {`${props.author.substr(0, 65)}`}</small></h4>
+            <h4>{`${props.title.substr(0, 50)}`}<br /><xsall>by {`${props.author.substr(0, 65)}`}</xsall></h4>
           </Link>
           <p>{props.summary}</p>
         </div>
-        <div className="col-md-2 queue-button-col">
-          <button
-            className="btn btn-primary"
-            onClick={props.addBookToQueue.bind(null, props.bookid)}
-          >
-            <span className="glyphicon glyphicon-plus">&nbsp;</span>
-            Add to Queue
-          </button>
+
+        <div className="col-xs-3">
+
+        <div className="row">
+          <div className="col-xs-12">
+            <button
+              className="btn btn-primary"
+              onClick={props.addBookToQueue.bind(null, props.bookid)}>
+
+              <span className="glyphicon glyphicon-plus">&nbsp;</span>
+              Add to Queue
+            </button>
+          </div>
         </div>
-        <div className="col-md-2 current-button-col">
-          <button
-            className="btn btn-primary"
-            onClick={props.makeCurrentBook.bind(null, props.bookid)}
-          >
-            <span className="glyphicon glyphicon-book">&nbsp;</span>
-            Make my Current
-          </button>
+
+        <div className="row">
+          <div className="col-xs-12">
+            <button
+              className="btn btn-primary"
+              onClick={props.makeCurrentBook.bind(null, props.bookid)}>
+
+              <span className="glyphicon glyphicon-book">&nbsp;</span>
+              Make my Current
+            </button>
+          </div>
         </div>
-      </div>
+        </div>
+
     </li>
+    </div>
   )
 }
 
