6b5e309afc74f5c4be12ec88451edac9fbb23deb
charlieperson <charlie.person@hyperisland.se>
2016-10-25 23:42:49 -0500

    Adds styling to queue dropdown

--- app/components/ProfileQueue.jsx
+++ app/components/ProfileQueue.jsx
@@ -6,17 +6,21 @@ const ProfileQueue = (props) => {
 // since the queue could get really long we only want to display up to 6 books
 const profileQueue = props.bookQueue.slice(0, 6).map((book, idx) => {
     return (
-      <Link to={`/books/${book._id}`}>
-        <img src={book.thumbnailPath} className='img-responsive bookPhoto'/>
-      </Link>
+      <div className="col-md-4">
+        <Link to={`/books/${book._id}`}>
+          <img src={book.thumbnailPath} className='bookPhoto'/>
+        </Link>
+      </div>
     );
   });
 
   console.log(props.bookQueue)
 
   return(
-    <DropdownButton title="Queue">
-      {profileQueue}
+    <DropdownButton title="Queue" className="droppy">
+      <div className="row">
+        {profileQueue}
+      </div>
     </DropdownButton>
   );
 
