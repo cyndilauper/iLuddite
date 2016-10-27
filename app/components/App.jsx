b22a5ad13955fabc6648a545b7eefd2888041081
Phil <plmok61@gmail.com>
2016-10-27 10:28:44 -0500

    rebasing from master

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -28,7 +28,7 @@ class App extends React.Component {
           this.setState({
             loggedInUser: response.data
           });
-          console.log(this.state.loggedInUser)
+          console.log('loggedInUser: ',this.state.loggedInUser)
           const path = `/users/${this.state.loggedInUser.fbid}`;
           browserHistory.push(path);
         });
