50df956795dfe05a3b2c80daa9ceb78cde3bce7d
Phil <plmok61@gmail.com>
2016-10-28 16:13:10 -0500

    search-styling

--- app/components/SearchListItem.jsx
+++ app/components/SearchListItem.jsx
@@ -5,7 +5,7 @@ const SearchListItem = (props) => {
   return (
     <li className="search-list-item list-group-item">
       <div className="row">
-        <div className="col-md-1">
+        <div className="col-md-2">
           <Link to={`/books/${props.bookid}`}>
             <img src={props.image} alt={props.title}/>
           </Link>
@@ -16,41 +16,40 @@ const SearchListItem = (props) => {
           </Link>
           <p>{props.summary}</p>
         </div>
-        <div className="col-md-2 queue-button-col">
-          <button
-            className="btn btn-primary"
-            onClick={props.addBookToQueue.bind(null, props.bookid)}
-          >
-            <span className="glyphicon glyphicon-plus">&nbsp;</span>
-            Add to Queue
-          </button>
-        </div>
-        <div className="col-md-2 current-button-col">
-          <button
-            className="btn btn-primary"
-            onClick={props.makeCurrentBook.bind(null, props.bookid)}
-          >
-            <span className="glyphicon glyphicon-book">&nbsp;</span>
-            Make my Current
-          </button>
-        </div>
-        <div className="col-md-2 current-button-col">
-          <button
-            className="btn btn-primary"
-            onClick={props.addBookToPastReads.bind(null, props.bookid)}
-          >
-            <span className="glyphicon glyphicon-book">&nbsp;</span>
-            Add to Past Reads
-          </button>
-        </div>
-        <div className="col-md-2 current-button-col">
-          <button
-            className="btn btn-primary"
-            onClick={props.addBookToFavorites.bind(null, props.bookid)}
-          >
-            <span className="glyphicon glyphicon-book">&nbsp;</span>
-            Add to Favorites
-          </button>
+        <div className="col-md-3">
+          
+            <button
+              className="btn btn-primary"
+              onClick={props.addBookToQueue.bind(null, props.bookid)}
+            >
+              <span className="glyphicon glyphicon-plus">&nbsp;</span>
+              Add to Queue
+            </button>
+          
+            <button
+              className="btn btn-primary"
+              onClick={props.makeCurrentBook.bind(null, props.bookid)}
+            >
+              <span className="glyphicon glyphicon-book">&nbsp;</span>
+              Make my Current
+            </button>
+          
+            <button
+              className="btn btn-primary"
+              onClick={props.addBookToPastReads.bind(null, props.bookid)}
+            >
+              <span className="glyphicon glyphicon-book">&nbsp;</span>
+              Add to Past Reads
+            </button>
+          
+            <button
+              className="btn btn-primary"
+              onClick={props.addBookToFavorites.bind(null, props.bookid)}
+            >
+              <span className="glyphicon glyphicon-book">&nbsp;</span>
+              Add to Favorites
+            </button>
+         
         </div>
       </div>
     </li>
