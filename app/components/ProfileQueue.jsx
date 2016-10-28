c5b121dd89bb30721a79d0d0e584d73f8d5bfdb1
charlieperson <charlie.person@hyperisland.se>
2016-10-28 11:48:17 -0500

    Incorporates masonry styling to queue drop down

--- app/components/ProfileQueue.jsx
+++ app/components/ProfileQueue.jsx
@@ -1,29 +1,39 @@
 const React = require('react');
 import { Link } from 'react-router';
 import { DropdownButton } from 'react-bootstrap';
+var Masonry = require('react-masonry-component');
 
-const ProfileQueue = (props) => {
-// since the queue could get really long we only want to display up to 6 books
-const profileQueue = props.bookQueue.slice(0, 6).map((book, idx) => {
-    return (
-      <div className="col-md-4">
-        <Link to={`/books/${book._id}`}>
-          <img src={book.thumbnailPath} className='bookPhoto'/>
-        </Link>
-      </div>
-    );
-  });
-
-  console.log(props.bookQueue)
-
-  return(
-    <DropdownButton title="Queue" className="droppy">
-      <div className="row">
-        {profileQueue}
-      </div>
-    </DropdownButton>
-  );
-
+var masonryOptions = {
+    transitionDuration: 0
 };
+
+const ProfileQueue = React.createClass({
+
+    render: function () {
+      const profileQueue = this.props.bookQueue.slice(0, 6).map((book, idx) => {
+        return (
+          <li className="image-element-class">
+            <Link to={`/books/${book._id}`}>
+              <img src={book.thumbnailPath} className='bookPhoto'/>
+            </Link>
+          </li>
+        );
+      });
+
+      return (
+          <DropdownButton title="Queue" className="droppy">
+            <Masonry
+                className={'my-gallery-class'} // default ''
+                elementType={'ul'} // default 'div'
+                options={masonryOptions} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+            >
+                {profileQueue}
+            </Masonry>
+          </DropdownButton>
+      );
+    }
+});
 
 module.exports = ProfileQueue;
